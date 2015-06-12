3f80103b1b0d9b736c198e36765063447f222ba9
Jonathan Tien <jonathan.tien@gmail.com>
2015-06-12 14:22:07 -0700

    replace manual API call with geopy library

--- fetch_data.py
+++ fetch_data.py
@@ -1,19 +1,13 @@
-import os, requests, json, datetime, logging
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodtrucks.settings")
+import os; os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodtrucks.settings")
+
+import requests, json, datetime, logging, geopy
+from geopy.geocoders import GoogleV3
 
 from trucks.models import Truck
 
 api_url = 'https://data.sfgov.org/resource/6a9r-agq8.json?$$exclude_system_fields=false&status=APPROVED'
 headers = {'X-App-Token': 'mbB8LuRTwTwODNOcQNYLEKCXJ'}
-
-geocoding_api = "https://maps.googleapis.com/maps/api/geocode/json"
-geocoding_key = 'AIzaSyCPlyfyEB3jUWgmlxnSeDkFvm6xPcF8Gsg'
-
-def get_latlong(addr):
-    addr += " San Francisco, CA"
-    address = "+".join(addr.split())
-    r = requests.get(geocoding_api+"?key="+geocoding_key+"&address="+address)
-    return r.json()['results'][0]['geometry']['location'].values()
+geolocator = GoogleV3()
 
 r = requests.get(api_url, headers=headers)
 
@@ -33,7 +27,8 @@ else:
             truck.lat = item['latitude']
             truck.long = item['longitude']
         except KeyError:
-            truck.lat, truck.long = get_latlong(item['address'])
+            location = geolocator.geocode(item['address']+ " San Francisco, CA")
+            truck.lat, truck.long = location.latitude, location.longitude
         trucks.append(truck)
 
     Truck.objects.bulk_create(trucks)
