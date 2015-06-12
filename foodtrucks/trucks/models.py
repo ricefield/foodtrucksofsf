7aca359915c9e6218c14e4f174e12390ea7213eb
Jonathan Tien <jonathan.tien@gmail.com>
2015-06-11 22:15:48 -0700

    add trucks model

--- foodtrucks/trucks/models.py
+++ foodtrucks/trucks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Truck(models.Model):
+
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    food = models.TextField()
+    lat = models.FloatField()
+    long = models.FloatField()
